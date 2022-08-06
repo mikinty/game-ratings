e6edef271b8b71021fd376aaa41beaa5aef3dd5b
Michael You <youmichaelc@gmail.com>
2022-08-06 18:51:50 -0400

    Fix bug with res.status not defined

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(require('./routes/record'));
 
 // Global error handling
-app.use(function (err, _req, res) {
+app.use(function (err, _req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
