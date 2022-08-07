3b959a6bdd2c2f61d5d83e0dc6b4dfaf95f6d2cf
Michael You <youmichaelc@gmail.com>
2022-08-06 20:20:34 -0400

    Setup Swagger docs and endpoint

--- server/server.js
+++ server/server.js
@@ -6,12 +6,30 @@ const cors = require('cors');
 // get MongoDB driver connection
 const dbo = require('./db/conn');
 
+// Swagger docs
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(require('./routes/record'));
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Aurory ChallengeAPI',
+      version: '1.0.0',
+      description: 'API for adding games and updating rankings of players via Glicko'
+    }
+  },
+  apis: ['routes/record.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 // Global error handling
 app.use(function (err, _req, res, next) {
