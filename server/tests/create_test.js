531f0c87981e002d31935eb49492c17ee2a9c07d
Michael You <youmichaelc@gmail.com>
2022-08-07 21:44:43 -0400

    Just more detail on the create test

--- server/tests/create_test.js
+++ server/tests/create_test.js
@@ -4,12 +4,14 @@ const record = require('../routes/record.js');
 const request = require('supertest');
 
 describe('Creating PlayerRating in DB', () => {
-    it('Creates a New PlayerRating (User)', (done) => {
+    it('Creates a New PlayerRating and then reads to make sure the data is accurate', (done) => {
         request(record)
             .post('/create')
             .send()
             .expect(204)
             .then(res => done())
             .catch(err => done(err));
+
+        // Check that the data is right. That is, losses, wins, draws are 0.
     });
 });
